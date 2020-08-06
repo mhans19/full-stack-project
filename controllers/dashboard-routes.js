d5388441c330d5289c78ffde718e95fd3c722a86
mhans19 <mdhansen19@gmail.com>
2020-08-06 17:34:49 -0600

    Set up controllers for home-routes (homepage) and dashboard-routes (dashboard page)

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,95 @@
+// DEPENDENCIES
+const router = require('express').Router();
+const withAuth = require('../utils/auth');
+const { Project, User, Contribution } = require('../models');
+// GET ALL FOR DASHBOARD
+router.get('/', withAuth, (req, res) => {
+  Project.findAll({
+    where: {
+      // use the ID from the session
+      user_id: req.session.user_id
+    },
+    attributes: [
+      'id',
+      'title',
+      'description',
+      'created_at'
+    ],
+    include: [
+      {
+        model: Contribution,
+            attributes: ['id', 
+                         'contribution_type', 
+                         'contribution_hours',
+                         'project_id', 
+                         'user_id', 
+                         'created_at'
+                        ],
+            include: {
+              model: User,
+              attributes: ['username']
+            }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbProjectData => {
+      // serialize data before passing to template
+      const projects = dbProjectData.map(project => project.get({ plain: true }));
+      res.render('dashboard', { projects, loggedIn: true });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get('/edit/:id', withAuth, (req, res) => {
+  Project.findOne({
+      where: {
+          id: req.params.id
+      },
+      attributes: [
+          'id',
+          'title',
+          'description',
+          'created_at'
+      ],
+      include: [{
+                model: Contribution,
+                attributes: ['id', 
+                            'contribution_type', 
+                            'contribution_hours',
+                            'project_id', 
+                            'user_id', 
+                            'created_at'
+                            ],
+                include: {
+                model: User,
+                attributes: ['username']
+                }
+          },
+          {
+              model: User,
+              attributes: ['username']
+          }
+      ]
+  })
+  .then(dbProjectData => {
+      const project = dbProjectData.get({ plain: true });
+  
+      res.render('edit-project', {
+          project,
+          loggedIn: true
+      });
+  })
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
+});
+// EXPORT MODULE
+module.exports = router;
---
+++ controllers/home-routes.js
@@ -0,0 +1,123 @@
+//DEPENDENCIES
+const router = require('express').Router();
+const { Project, User, Contribution } = require('../models');
+// GET ALL
+router.get('/', (req, res) => {
+    console.log(req.session);
+    Project.findAll({
+      attributes: [
+        'id',
+        'title',
+        'description',
+        'created_at'
+      ],
+      include: [
+        {
+          model: Contribution,
+          attributes: ['id', 
+                       'contribution_type', 
+                       'contribution_hours',
+                       'project_id', 
+                       'user_id', 
+                       'created_at'
+                      ],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbProjectData => {
+        const projects = dbProjectData.map(project => project.get({ plain: true }));
+        // pass a single project object into the homepage template
+        res.render('homepage', {
+          projects,
+          loggedIn: req.session.loggedIn
+        });
+        router.get('/login', (req, res) => {
+          res.render('login');
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+// GET LOGIN
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+// GET SIGNUP
+router.get('/signup', (req, res) => {
+  if(req.session.loggedIn) {
+      res.redirect('/');
+      return;
+  }
+  
+  res.render('sign-up');
+});
+// GET ONE PROJECT  
+  router.get('/project/:id', (req, res) => {
+    Project.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'id',
+        'title',
+        'description',
+        'created_at'
+      ],
+      include: [
+        {
+            model: Contribution,
+            attributes: ['id', 
+                         'contribution_type', 
+                         'contribution_hours',
+                         'project_id', 
+                         'user_id', 
+                         'created_at'
+                        ],
+            include: {
+              model: User,
+              attributes: ['username']
+            }
+        },
+        {
+            model: User,
+            attributes: ['username']
+        }
+      ]
+    })
+      .then(dbProjectData => {
+        if (!dbProjectData) {
+          res.status(404).json({ message: 'No project found with this id' });
+          return;
+        }
+  
+        // serialize the data
+        const post = dbProjectData.get({ plain: true });
+  
+        // pass data to template
+        res.render('single-project', {
+          post,
+          loggedIn: req.session.loggedIn
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+// MODULE EXPORTS  
+  module.exports = router;
