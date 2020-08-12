871b7ff791f92e8268873eec3f7cd8e2f0617792
Theodore Butts <theodorebutts@gmail.com>
2020-08-11 19:57:03 -0600

    merge with Drake;s edits

--- config/connection.js
+++ config/connection.js
@@ -12,7 +12,7 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: process.env.DB_HOST || "localhost",
     dialect: 'mysql',
-    host: process.env.DB_PORT || 3306
+    port: process.env.DB_PORT || 3306
   });
 }
 
