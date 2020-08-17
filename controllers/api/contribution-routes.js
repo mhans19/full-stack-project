1d5d7ce0eadd56e8e93eb17cf7028bba1814f59b
mhans19 <mdhansen19@gmail.com>
2020-08-17 17:30:35 -0600

    re-initiate package.json

--- controllers/api/contribution-routes.js
+++ controllers/api/contribution-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Contribution } = require('../../models/contibution-routes');
+const { Project, User, Contribution } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.get('/', (req, res) => {
