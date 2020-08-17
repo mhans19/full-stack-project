8fd916ac2b249fa9c26c3516682852670f4276ef
mhans19 <mdhansen19@gmail.com>
2020-08-17 17:21:27 -0600

    Attempt to fix model path for Heroku

--- controllers/api/contribution-routes.js
+++ controllers/api/contribution-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Contribution } = require('../../models');
+const { Contribution } = require('../../models/contibution-routes');
 const withAuth = require('../../utils/auth');
 
 router.get('/', (req, res) => {
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Project, Contribution } = require("../../models");
+const { Project, User, Contribution } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 // GET /api/users
